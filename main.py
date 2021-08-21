415c819d1ca3c9b5540bfa9aa53057d790e2fd7e
Pas2704 <71668530+Pas2704@users.noreply.github.com>
2021-08-20 23:50:29 -0400

    Fixed bug in time_until_str, dummy proofed date input, added try/except for any errors that are found

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from datetime import date, time, datetime, timedelta, tzinfo
-import time as just_time
+from datetime import date, time, datetime
 import math
 import re
+from calendar import monthrange
+import traceback
 
 
 def get_date():
@@ -28,6 +29,18 @@ def get_date():
                 f"Error, this program does not support a year as high as {year}. Please try again"
             )
             continue
+        if month < 1 or month > 12:
+            print(
+                "Error: month must be a whole number between 1 and 12. Please try again."
+            )
+            continue
+        max_date = monthrange(year, month)[1]
+        if day < 1 or day > max_date:
+            print(
+                f"Error: date must be a whole number between 1 and the max date for this month/year ({max_date}). Please try again."
+            )
+            continue
+
         return date(year, month, day)
 
 
@@ -37,7 +50,7 @@ def check_for_non_numbers(split_input):
         try:
             int(num)
         except:
-            print(f'Error: "{num}" is not a number. Please try again.')
+            print(f'Error: "{num}" is not a whole number. Please try again.')
             Error = True
             break
     return Error
@@ -68,7 +81,7 @@ def time_until_str(target_datetime):
     if seconds >= 86400:
         return "in a day"
     if seconds >= 7200:
-        return f"in {round(seconds/360)} hours"
+        return f"in {round(seconds/3600)} hours"
     if seconds >= 3600:
         return "in an hour"
     if seconds >= 120:
@@ -80,7 +93,7 @@ def time_until_str(target_datetime):
     if seconds <= -86400:
         return "in a day"
     if seconds <= -7200:
-        return f"{abs(round(seconds/360))} hours ago"
+        return f"{abs(round(seconds/3600))} hours ago"
     if seconds <= 3600:
         return "in an hour"
     if seconds <= -120:
@@ -91,27 +104,47 @@ def time_until_str(target_datetime):
 
 
 if __name__ == "__main__":
-    target = datetime.combine(get_date(), get_time())
-    print("---------------------RESULT----------------------------------------------")
-    print("Input to Discord:              Example Result (what discord will display):")
-    print(
-        f"<t:{math.trunc(target.timestamp())}:R>               {time_until_str(target)}"
-    )  # 4 minutes ago\in 4 minutes")
-    print(
-        f"<t:{math.trunc(target.timestamp())}:d>               {target.strftime('%m/%d/%Y')}"
-    )  # 08/20/2021")
-    print(
-        f"<t:{math.trunc(target.timestamp())}:D>               {target.strftime('%B %d, %Y')}"
-    )  # August 20, 2021")
-    print(
-        f"<t:{math.trunc(target.timestamp())}:t>               {target.strftime('%I:%M %p')}"
-    )  # 8:49 PM")
-    # print(f"<t:{math.trunc(target.timestamp())}:T>               {target.strftime('%-I:%M:%S %p')}") # 8:49:00 PM")
-    print(
-        f"<t:{math.trunc(target.timestamp())}:f>               {target.strftime('%B %d, %Y %I:%M %p')}"
-    )  # August 20, 2021 8:49 PM")
-    print(
-        f"<t:{math.trunc(target.timestamp())}:F>               {target.strftime('%A, %B %d, %Y %I:%M %p')}"
-    )  # Friday, August 20, 2021 8:49 PM")
-
-    exit = input("Enter to Exit")
+    try:
+        target = datetime.combine(get_date(), get_time())
+        print(
+            "---------------------RESULT----------------------------------------------"
+        )
+        print(
+            "Input to Discord:              Example Result (what discord will display):"
+        )
+        print(
+            f"<t:{math.trunc(target.timestamp())}:R>               {time_until_str(target)}"
+        )  # 4 minutes ago\in 4 minutes")
+        print(
+            f"<t:{math.trunc(target.timestamp())}:d>               {target.strftime('%m/%d/%Y')}"
+        )  # 08/20/2021")
+        print(
+            f"<t:{math.trunc(target.timestamp())}:D>               {target.strftime('%B %d, %Y')}"
+        )  # August 20, 2021")
+        print(
+            f"<t:{math.trunc(target.timestamp())}:t>               {target.strftime('%I:%M %p')}"
+        )  # 8:49 PM")
+        # print(f"<t:{math.trunc(target.timestamp())}:T>               {target.strftime('%-I:%M:%S %p')}") # 8:49:00 PM")
+        print(
+            f"<t:{math.trunc(target.timestamp())}:f>               {target.strftime('%B %d, %Y %I:%M %p')}"
+        )  # August 20, 2021 8:49 PM")
+        print(
+            f"<t:{math.trunc(target.timestamp())}:F>               {target.strftime('%A, %B %d, %Y %I:%M %p')}"
+        )  # Friday, August 20, 2021 8:49 PM")
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt (ctrl+c), program shutting down")
+    except Exception as e:
+        print("\nHmm, you ran into an error I haven't found yet")
+        print(
+            "DM @[DSI] Pas2704#1283 on discord with a screenshot of the following so I can make a fix:"
+        )
+        print(
+            "------------------------------------------------------------------------------------------"
+        )
+        print(repr(e))
+        traceback.print_exc()
+        print(
+            "------------------------------------------------------------------------------------------"
+        )
+    finally:
+        exit = input("Enter to Exit")
